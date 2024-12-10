b9bd88261ee8438f0987be5a6fd95269c09fcfdc
BeegGiraffe <jeanmishoo@gmail.com>
2024-12-10 17:00:03 -0500

    user register

--- wwwroot/Posts/js/siteUI.js
+++ wwwroot/Posts/js/siteUI.js
@@ -250,7 +250,6 @@ function renderPost(post, loggedUser) {
 
     return $(`
         <div class="post" id="${post.Id}">
-            <div>${global.sessionStorage.getItem("token").user.Name}</div>
             <div class="postHeader">
                 ${post.Category}
                 ${crudIcon}
